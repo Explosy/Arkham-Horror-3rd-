d4b19a512e1e1c74fb58dd627d58cc1fc12da4dd
amoroz <amoroz@onellect.ru>
2023-07-14 12:45:56 +0300

    INFRASTRUCTURE: initialize core unit test
    Closes #2

---
+++ Explosy.ArkhamHorror3rd.Core.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,10 @@
+using Xunit;
+
+namespace Explosy.ArkhamHorror3rd.Core.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
